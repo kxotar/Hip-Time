015435bc002599ae513ebeb22b1440b1a39c4a05
Phantom <sharma26sh@yahoo.in>
2016-06-07 04:47:31 +0530

    Add Completed Ability

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,11 @@ def destroy
   @item.destroy
   redirect_to root_path
 end
+def complete
+  @item= Item.find(params[:id])
+      @item.update_attribute(:completed_at, Time.now)
+      redirect_to root_path
+end
   private
   def item_params
     params.require(:item).permit(:title, :description)
